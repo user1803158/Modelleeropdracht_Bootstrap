7afe8ff30cef56b4ae717925a8ec795f220b7bd9
koenbres04 <koenbres@gmail.com>
2023-03-01 15:03:32 +0100

    Almost done with parametric bootstrap with CIs.

--- confidence_intervals.r
+++ confidence_intervals.r
@@ -1,9 +1,10 @@
 
 
+confidence_interval_names = c("quantile_based")
 
 intervals <- function(bootstrap_data, alpha){
   list(
-    quantile_based=C(quantile(bootstrap_data, alpha/2), quantile(bootstrap_data, 1-alpha/2)),
+    c(quantile(bootstrap_data, alpha/2), quantile(bootstrap_data, 1-alpha/2))
   )
 }
 
--- monster_functie.r
+++ monster_functie.r
@@ -1,4 +1,4 @@
-
+library("plyr")
 source("confidence_intervals.r")
 
 
@@ -11,19 +11,39 @@ nonparam_bootstrap_data <- function(data, N, estimator){
 }
 
 
-param_monster <- function (true_distr_gen, true_params, params_estimator, n, M, N, alphas=NULL, estimator=NULL, bootstrap_distr_gen=NULL){
+param_monster <- function (true_distr_gen, true_params, true_theta, params_estimator, n, M, N, true_variance_N, alphas=c(), estimator=NULL, bootstrap_distr_gen=NULL){
   if (is.null(estimator)){
     estimator = params_estimator
   }
   if (is.null(bootstrap_distr_gen)){
     bootstrap_distr_gen = true_distr_gen
   }
+  
+  # calculate the true varaince
+  true_variance = var(param_bootstrap_data(true_distr_gen(true_params), n, true_variance_N, estimator))
+  
+  # apply the bootstrap algorithm M times and put some results in the 'bootstrap_results' dataframe  
   true_distr = true_distr_gen(true_params)
-  replicate(M, {
+  bootstrap_results = rdply(M, {
     params_hat = params_estimator(true_distr(n))
     data = param_bootstrap_data(bootstrap_distr_gen(params_hat), n, N, estimator)
-    var(data)
-  })
+    interval_pairs = unlist(lapply(alphas, function(x) intervals(data, x)), recursive=F)
+    truth_values = vapply(interval_pairs, function(x) (x[[1]] <= true_theta) & (true_theta <= x[[2]]), numeric(1))
+    c(var(data), truth_values)
+  }, .id=NULL)
+  
+  # turn bootstrap_results into a nice results list
+  results = list(mean((bootstrap_results[,1]-true_variance)^2))
+  for (i in 0:(length(alphas)-1)){
+    for (j in 0:(length(confidence_interval_names)-1)){
+      collumn_index = 1+i*length(alphas)+j
+      results = c(results, mean(bootstrap_results[, collumn_index]))
+    }
+  }
+  result_names = c("mean_square_error_of_variance")
+  # TODO suff here
+  names(results) = result_names
+  results
 }
 
 nonparam_monster <- function(true_distr, estimator, n, M, N){
