b246636fd78a0d5066626512fb45ad53c6e0be94
koenbres04 <koenbres@gmail.com>
2023-03-08 18:02:54 +0100

    Moved code form Confidence_intervals.Rmd to confidence_intervals.r

--- confidence_intervals.r
+++ confidence_intervals.r
@@ -1,10 +1,95 @@
 
 
-confidence_interval_names = c("quantile_based")
 
-intervals <- function(bootstrap_data, alpha){
+stderror <- function(x) sd(x)/sqrt(length(x))
+
+BCa <- function(bootstrap_estimates, bootstrap_sderrors, theta_hat, start_sample, sample_sderror, estimator, alpha) {
+  B <- length(bootstrap_estimates)
+  N <- length(start_sample)
+  number <- 0
+  # x <- list(sample)
+  theta <- rep(0, N)
+  
+  for(i in 1:B){
+    if (bootstrap_estimates[i] < theta_hat){
+      number <- number + 1
+    }
+  }
+  
+  z0 <- qnorm(number/B)
+  
+  for(i in 1:N)
+  {
+    x_i <- start_sample[-i]
+    theta[i] <- estimator(x_i)
+  }
+  
+  theta_mean <- mean(theta)
+  
+  teller <- (theta_mean - theta)^3
+  noemer <- (theta_mean - theta)^2
+  
+  a <- sum(teller)/(6*sum(noemer)^(3/2))
+  
+  alpha_1 <- pnorm(z0 + (z0 + qnorm(alpha/2))/(1 - a*(z0 + qnorm(alpha/2))))
+  alpha_2 <- pnorm(z0 + (z0 + qnorm(1-alpha/2))/(1 - a*(z0 + qnorm(1-alpha/2))))
+  
+  bootstrap_estimates <- sort(bootstrap_estimates)
+  
+  theta_lo <- bootstrap_estimates[ceiling(B * alpha_1)]
+  theta_up <- bootstrap_estimates[floor(B * alpha_2)]
+  
+  CI <- c(theta_lo, theta_up)
+  
+  return(CI)
+}
+
+Bootstrap_t <- function(bootstrap_estimates, bootstrap_sderrors, theta_hat, sample_sderror, alpha) {
+  B <- length(bootstrap_estimates)
+  Z <- rep(0, B)
+  
+  for(i in 1:B)
+    Z[i] <- (bootstrap_estimates[i]-theta_hat)/bootstrap_sderrors[i]
+  
+  Z <- sort(Z)
+  
+  h1 <- ceiling(B * alpha/2)
+  h2 <- floor(B * (1-alpha/2))
+  
+  t1 <- Z[h1]
+  t2 <- Z[h2]
+  
+  theta_lo <- theta_hat-t2*sample_sderror
+  theta_up <- theta_hat-t1*sample_sderror
+  
+  CI <- c(theta_lo, theta_up)
+  
+  return(CI)
+}   
+
+
+Basic <- function(bootstrap_estimates, bootstrap_sderrors, theta_hat, sample_sderror, alpha) {
+  B <- length(bootstrap_estimates)
+  
+  h1 <- ceiling(B * alpha/2)
+  h2 <- floor(B * (1-alpha/2))
+  
+  bootstrap_estimates = sort(bootstrap_estimates)
+  
+  theta_lo <- bootstrap_estimates[h1]
+  theta_up <- bootstrap_estimates[h2]
+  
+  CI <- c(theta_lo, theta_up)
+  
+  return(CI)
+}
+
+confidence_interval_names = c("BCa", "Bootstrap_t", "Basic")
+intervals <- function(bootstrap_estimates, bootstrap_sderrors, theta_hat, start_sample, sample_sderror, estimator, alpha){
   list(
-    c(quantile(bootstrap_data, alpha/2), quantile(bootstrap_data, 1-alpha/2))
+    BCa(bootstrap_estimates, bootstrap_sderrors, theta_hat, start_sample, sample_sderror, estimator, alpha),
+    Bootstrap_t(bootstrap_estimates, bootstrap_sderrors, theta_hat, sample_sderror, alpha),
+    Basic(bootstrap_estimates, bootstrap_sderrors, theta_hat, sample_sderror, alpha)
   )
 }
 
--- monster_functie.r
+++ monster_functie.r
@@ -6,18 +6,16 @@ param_bootstrap_data <- function(distr, n, N, estimator){
   replicate(N, estimator(distr(n)))
 }
 
-nonparam_bootstrap_data <- function(data, N, estimator){
-  replicate(N, estimator(sample(data, length(data), replace=T)))
-}
 
-
-analyse_bootstrap_data <- function(true_theta, alphas, true_variance, M, bootstrap_data_generator){
+analyse_bootstrap_data <- function(true_theta, alphas, true_variance, M, estimator, bootstrap_data_generator){
   # apply the bootstrap algorithm M times and put some results in the 'bootstrap_results' dataframe
   bootstrap_results = rdply(M, .id=NULL, .expr={
-    data = bootstrap_data_generator()
-    interval_pairs = unlist(lapply(alphas, function(x) intervals(data, x)), recursive=F)
-    truth_values = vapply(interval_pairs, function(x) (x[[1]] <= true_theta) & (true_theta <= x[[2]]), numeric(1))
-    c(var(data), truth_values)
+    bootstrap_data = bootstrap_data_generator()
+    interval_pairs = unlist(lapply(alphas, function(alpha){
+      intervals(bootstrap_data$estimates, bootstrap_data$stderrors, bootstrap_data$theta_hat, bootstrap_data$start_sample, bootstrap_data$sample_stderror, estimator, alpha)
+    }), recursive=F)
+    truth_values = vapply(interval_pairs, function(x) (x[1] <= true_theta) & (true_theta <= x[2]), numeric(1))
+    c(var(bootstrap_data$estimates), truth_values)
   })
   
   # turn bootstrap_results into a nice results list
@@ -31,7 +29,7 @@ analyse_bootstrap_data <- function(true_theta, alphas, true_variance, M, bootstr
   result_names = c("mean square error of variance")
   for (alpha in alphas){
     for (ci_name in confidence_interval_names){
-      name = paste(ci_name, "alpha =", format(round(alpha, digits=3), nsmall=3))
+      name = paste(ci_name, ";alpha=", format(round(alpha, digits=3), nsmall=3), sep="")
       result_names = c(result_names, name)
     }
   }
@@ -50,14 +48,37 @@ param_monster <- function (true_distr_gen, true_params, true_theta, params_estim
   # calculate the true variance
   true_distr = true_distr_gen(true_params)
   true_variance = var(param_bootstrap_data(true_distr, n, true_variance_N, estimator))
-  analyse_bootstrap_data(true_theta, alphas, true_variance, M, function(){
-    param_bootstrap_data(bootstrap_distr_gen(params_estimator(true_distr(n))), n, N, estimator)
+  analyse_bootstrap_data(true_theta, alphas, true_variance, M, estimator, function(){
+    start_sample = true_distr(n)
+    distr = bootstrap_distr_gen(params_estimator(start_sample))
+    bootstrap_results = raply(N, {
+      bootstrap_sample = distr(n)
+      c(estimator(bootstrap_sample), stderror(bootstrap_sample))
+    })
+    list(
+      theta_hat=estimator(start_sample),
+      sample_stderror=stderror(start_sample),
+      start_sample=start_sample,
+      estimates=bootstrap_results[,1], 
+      stderrors=bootstrap_results[,2]
+    )
   })
 }
 
 nonparam_monster <- function(true_distr, estimator, true_theta, n, M, N, true_variance_N, alphas=c()){
-  true_variance = var(param_bootstrap_data(true_distr, n, true_variance_N ,estimator))
-  analyse_bootstrap_data(true_theta, alphas, true_variance, M, function(){
-    nonparam_bootstrap_data(true_distr(n), N, estimator)
+  true_variance = var(param_bootstrap_data(true_distr, n, true_variance_N , estimator))
+  analyse_bootstrap_data(true_theta, alphas, true_variance, M, estimator, function(){
+    start_sample = true_distr(n)
+    bootstrap_results = raply(N, {
+      bootstrap_sample = sample(start_sample, n, replace=T)
+      c(estimator(bootstrap_sample), stderror(bootstrap_sample))
+    })
+    list(
+      theta_hat=estimator(start_sample),
+      sample_stderror=stderror(start_sample),
+      start_sample=start_sample,
+      estimates=bootstrap_results[,1], 
+      stderrors=bootstrap_results[,2]
+    )
   })
 }
